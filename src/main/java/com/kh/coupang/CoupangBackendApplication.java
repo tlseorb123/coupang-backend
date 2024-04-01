db1b0538e317b3ddaa3ea1b450a5d7a9b6a13fd2
tlseorb123 <152463286+tlseorb123@users.noreply.github.com>
2024-04-01 09:46:44 +0900

    Initial commit

---
+++ src/main/java/com/kh/coupang/CoupangBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kh.coupang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupangBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoupangBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/coupang/CoupangBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.coupang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupangBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
